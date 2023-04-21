3fa9295ba48634f216ef4be9fe1e356f51204a25
Krishi Sharma <krishisharma45@gmail.com>
2023-04-21 11:23:04 -0400

    add additional server side config and averaging function for federated evaluation

--- src/server.py
+++ src/server.py
@@ -1,9 +1,35 @@
+from typing import List, Tuple, Any
+
 import flwr as fl
 
 from src.config.server_config import ServerConfig, Config
 
+
+class FlowerServer:
+    def __init__(self, server_config: ServerConfig):
+        self.server_config = server_config
+
+    def run(self):
+        fl.server.start_server(server_address=f"{self.server_config.server_address}:8080",
+                               config={**Config().__dict__},
+                               strategy=self._strategy())
+
+    def _strategy(self):
+        return fl.server.strategy.FedAvg(
+            min_available_clients=self.server_config.min_available_clients,
+            min_fit_clients=self.server_config.min_fit_clients,
+            min_eval_clients=self.server_config.min_eval_clients,
+            evaluate_metrics_aggregation_fn=self.weighted_average
+        )
+
+    @staticmethod
+    def weighted_average(metrics: List[Tuple[int, Any]]) -> Any:
+        accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+        examples = [num_examples for num_examples, _ in metrics]
+        return {"accuracy": sum(accuracies) / sum(examples)}
+
+
 if __name__ == "__main__":
     server_config = ServerConfig()
-    fl.server.start_server(server_address=f"{server_config.server_address}:8080",
-                           config={**Config().__dict__},
-                           strategy=fl.server.strategy.FedAvg(min_available_clients=server_config.min_available_clients))
+    flower_server = FlowerServer(server_config=server_config)
+    flower_server.run()
